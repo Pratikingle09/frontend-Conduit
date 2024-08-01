2f3e6b788345a566bcbf34a3a514eb37d2f2b673
Pratik Ingle <pratikingle66@gmail.com>
2024-08-01 16:59:10 +0530

    Update deleteArticle.js

--- src/services/deleteArticle.js
+++ src/services/deleteArticle.js
@@ -8,7 +8,6 @@ async function deleteArticle({ slug, headers }) {
       method: "DELETE",
       url: `https://new-deployment-project.onrender.com/api/articles/${slug}/`,
     });
-
     return data;
   } catch (error) {
     errorHandler(error);
